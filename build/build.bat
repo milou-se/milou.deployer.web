6503bea574404587ad371e2997baa6d85b59e8fe
Niklas Lundberg <info@nlundberg.se>
2018-06-05 10:13:49 +0200

    Enabled xunit in build.

--- build/build.bat
+++ build/build.bat
@@ -9,6 +9,7 @@ SET Arbor.X.Build.VariableOverrideEnabled=true
 SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=true
 SET Arbor.X.Build.NetAssembly.Configuration=
 SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
+SET Arbor.X.Tools.External.Xunit.NetCoreApp.Enabled=true
 
 SET Fallback.Version.Build=0
 
