f3d1b4ab0ebb34cf9e5ca47117023822dd9df6f9
Niklas Lundberg <info@nlundberg.se>
2018-10-10 23:07:55 +0200

    Updated NuGet packages.

---
+++ src/Milou.Deployer.Web.IisHost/Areas/Security/AllowedHostName.cs
@@ -0,0 +1,21 @@
+﻿using Arbor.KVConfiguration.Urns;
+using JetBrains.Annotations;
+using Milou.Deployer.Web.Core.Configuration;
+
+namespace Milou.Deployer.Web.IisHost.Areas.Security
+{
+    [Urn(HostNameUrn)]
+    [Optional]
+    [UsedImplicitly]
+    public class AllowedHostName
+    {
+        public const string HostNameUrn = "urn:milou:deployer:web:allowed-host";
+
+        public AllowedHostName(string hostName)
+        {
+            HostName = hostName;
+        }
+
+        public string HostName { get; }
+    }
+}
