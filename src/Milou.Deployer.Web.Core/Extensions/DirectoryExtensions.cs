2f2e27dac180fc404f629bbc6e8e5cbea78b2b54
Niklas Lundberg <info@nlundberg.se>
2018-10-25 15:48:44 +0200

    Added directory helper.

--- src/Milou.Deployer.Web.Core/Extensions/DirectoryExtensions.cs
+++ src/Milou.Deployer.Web.Core/Extensions/DirectoryExtensions.cs
@@ -4,8 +4,28 @@ using JetBrains.Annotations;
 
 namespace Milou.Deployer.Web.Core.Extensions
 {
-    public static  class DirectoryExtensions
+    public static class DirectoryExtensions
     {
+        public static bool TryEnsureDirectoryExists([NotNull] this string directory, out DirectoryInfo directoryInfo)
+        {
+            if (string.IsNullOrWhiteSpace(directory))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(directory));
+            }
+
+            try
+            {
+                directoryInfo = new DirectoryInfo(directory).EnsureExists();
+
+                return true;
+            }
+            catch (Exception ex) when (!ex.IsFatal())
+            {
+                directoryInfo = null;
+                return false;
+            }
+        }
+
         public static DirectoryInfo EnsureExists([NotNull] this DirectoryInfo directoryInfo)
         {
             if (directoryInfo == null)
