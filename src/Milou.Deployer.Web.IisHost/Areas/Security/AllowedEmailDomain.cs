ac51f1baa666d3c3233665bbd62028cc249a4aec
Niklas Lundberg <info@nlundberg.se>
2019-02-24 15:21:37 +0100

    Updated email domain

---
+++ src/Milou.Deployer.Web.IisHost/Areas/Security/AllowedEmailDomain.cs
@@ -0,0 +1,23 @@
+﻿using Arbor.KVConfiguration.Urns;
+using Milou.Deployer.Web.Core.Configuration;
+using Milou.Deployer.Web.Core.Validation;
+
+namespace Milou.Deployer.Web.IisHost.Areas.Security
+{
+    [Optional]
+    [Urn(UrnKey)]
+    public class AllowedEmailDomain : IConfigurationValues, IValidationObject
+    {
+        public const string UrnKey = "urn:milou:deployer:web:allowed-email-domain";
+
+        public AllowedEmailDomain(string domain)
+        {
+            Domain = domain;
+            IsValid = !string.IsNullOrWhiteSpace(domain);
+        }
+
+        public string Domain { get; }
+
+        public bool IsValid { get; }
+    }
+}
