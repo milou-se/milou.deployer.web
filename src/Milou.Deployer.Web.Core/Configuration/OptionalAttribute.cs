ee76291f7f6586c82e6f406a4dd4045ded025c18
Niklas Lundberg <info@nlundberg.se>
2019-04-17 22:07:45 +0200

    Updated packages.

--- src/Milou.Deployer.Web.Core/Configuration/OptionalAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Milou.Deployer.Web.Core.Configuration
-{
-    [AttributeUsage(AttributeTargets.Class)]
-    public sealed class OptionalAttribute : Attribute
-    {
-    }
-}
