ec0a250455fd68f83079f491cd68c62b1cc09894
Niklas Lundberg <niklas.lundberg@milou.se>
2019-05-02 11:53:52 +0200

    Added excluded files.

---
+++ src/Milou.Deployer.Web.Core/Deployment/Packages/PackageVersion.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using JetBrains.Annotations;
+using NuGet.Versioning;
+
+namespace Milou.Deployer.Web.Core.Deployment.Packages
+{
+    public sealed class PackageVersion : IEquatable<PackageVersion>
+    {
+        public PackageVersion(string packageId, SemanticVersion version)
+        {
+            if (version == null)
+            {
+                throw new ArgumentNullException(nameof(version));
+            }
+
+            if (string.IsNullOrWhiteSpace(packageId))
+            {
+                throw new ArgumentException("Argument is null or whitespace", nameof(packageId));
+            }
+
+            PackageId = packageId;
+            Version = version;
+            Key = $"{PackageId}_{Version.ToNormalizedString()}";
+        }
+
+        public string PackageId { get; }
+
+        public SemanticVersion Version { get; }
+
+        [PublicAPI]
+        public string Key { get; }
+
+        public static bool operator ==(PackageVersion left, PackageVersion right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(PackageVersion left, PackageVersion right)
+        {
+            return !Equals(left, right);
+        }
+
+        public bool Equals(PackageVersion other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return string.Equals(Key, other.Key, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return Equals((PackageVersion)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Key?.GetHashCode(StringComparison.InvariantCulture) ?? 0;
+        }
+
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
---
+++ src/Milou.Deployer.Web.Core/Deployment/Packages/PackageVersionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NuGet.Versioning;
+
+namespace Milou.Deployer.Web.Core.Deployment.Packages
+{
+    public static class PackageVersionExtensions
+    {
+        public static SemanticVersion Latest(this IEnumerable<PackageVersion> packageVersions)
+        {
+            return packageVersions.OrderByDescending(package => package.Version).FirstOrDefault()?.Version;
+        }
+    }
+}
