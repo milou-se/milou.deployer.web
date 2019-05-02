8cc9cd828223be904cc055ccc6ee2bcf69606b1a
Niklas Lundberg <info@nlundberg.se>
2019-05-02 22:00:35 +0200

    Stared Vue.js.

--- src/Milou.Deployer.Web.IisHost/Areas/Deployment/Controllers/MonitoringController.cs
+++ src/Milou.Deployer.Web.IisHost/Areas/Deployment/Controllers/MonitoringController.cs
@@ -6,6 +6,7 @@ using Milou.Deployer.Web.Core.Deployment.Sources;
 using Milou.Deployer.Web.Core.Extensions;
 using Milou.Deployer.Web.IisHost.Areas.Deployment.Services;
 using Milou.Deployer.Web.IisHost.Areas.Deployment.ViewOutputModels;
+using Milou.Deployer.Web.IisHost.Areas.Targets.Controllers;
 using Milou.Deployer.Web.IisHost.Controllers;
 
 namespace Milou.Deployer.Web.IisHost.Areas.Deployment.Controllers
@@ -33,15 +34,41 @@ namespace Milou.Deployer.Web.IisHost.Areas.Deployment.Controllers
         [Route("")]
         public async Task<IActionResult> Index(CancellationToken cancellationToken)
         {
-            var readOnlyCollection =
-                (await _targetSource.GetOrganizationsAsync(cancellationToken)).SelectMany(
+            var targets = (await _targetSource.GetOrganizationsAsync(cancellationToken))
+                .SelectMany(
                     organization => organization.Projects.SelectMany(project => project.DeploymentTargets))
                 .SafeToReadOnlyCollection();
 
             var appVersions =
-                await _monitoringService.GetAppMetadataAsync(readOnlyCollection, cancellationToken);
+                await _monitoringService.GetAppMetadataAsync(targets, cancellationToken);
 
             return View(new MonitoringViewOutputModel(appVersions));
+        }
+
+        [HttpGet]
+        [Route("~/status")]
+        public async Task<IActionResult> Status(CancellationToken cancellationToken)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("~/api/targets")]
+        public async Task<IActionResult> Targets(CancellationToken cancellationToken)
+        {
+            var targets = (await _targetSource.GetOrganizationsAsync(cancellationToken))
+                .SelectMany(
+                    organization => organization.Projects.SelectMany(project => project.DeploymentTargets))
+                .Select(t => new
+                {
+                    targetId = t.Id,
+                    name = t.Name,
+                    url = t.Url,
+                    editUrl = Url.RouteUrl(TargetConstants.EditTargetRouteName, new { deploymentTargetId = t.Id }),
+                    historyUrl = Url.RouteUrl(DeploymentConstants.HistoryRouteName, new { deploymentTargetId = t.Id })
+                });
+
+            return Json(new { targets = targets });
         }
     }
 }
---
+++ src/Milou.Deployer.Web.IisHost/wwwroot/Scripts/Status.js
@@ -0,0 +1,39 @@
+﻿class DeploymentTarget {
+    constructor(targetId, name, url, editUrl, historyUrl) {
+        this.targetId = targetId;
+        this.name = name;
+        this.url = url;
+        this.editUrl = editUrl;
+        this.historyUrl = historyUrl;
+    }
+    static from(json) {
+        return Object.assign(new DeploymentTarget(), json);
+    }
+}
+
+async function getTargets() {
+
+    let response = await fetch('/api/targets');
+
+    console.dir(response);
+
+    let json = await response.json();
+
+    let targets = json.targets.map(target => DeploymentTarget.from(target));
+
+    return targets;
+}
+
+let app = {};
+
+async function buildApp() {
+    let targets = await getTargets();
+    app = new Vue({
+        el: '#app',
+        data: {
+            targets: targets
+        }
+    });
+}
+
+buildApp();
