bd9aa1a1ca7aa5fb67ab381ff7024807bac25747
Niklas Lundberg <info@nlundberg.se>
2019-05-05 20:01:59 +0200

    Improved time ago.

--- src/Milou.Deployer.Web.Core/Time/DateTimeExtensions.cs
+++ src/Milou.Deployer.Web.Core/Time/DateTimeExtensions.cs
@@ -77,31 +77,37 @@ namespace Milou.Deployer.Web.Core.Time
         [PublicAPI]
         public static string Since(this DateTime to, DateTime from)
         {
+            string PluralSuffix(int count) => count > 1 ? "s" : "";
+
             var diff = to - from;
+
+            var diffTotalDays = (int)diff.TotalDays;
 
             if (diff.TotalDays > 365)
             {
-                return (int)diff.TotalDays + " days ago";
+                return $"{diffTotalDays} day{PluralSuffix(diffTotalDays)} ago";
             }
 
             if (diff.TotalDays > 30)
             {
-                return (int)diff.TotalDays / 30 + " months ago";
+                return $"{diffTotalDays / 30} month{PluralSuffix(diffTotalDays)} ago";
             }
 
             if (diff.TotalDays > 1)
             {
-                return (int)diff.TotalDays + " days ago";
+                return $"{diffTotalDays} day{PluralSuffix(diffTotalDays)} ago";
             }
 
             if (diff.TotalHours > 1)
             {
-                return (int)diff.TotalHours + " hours ago";
+                var diffTotalHours = (int)diff.TotalHours;
+                return $"{diffTotalHours} hour{PluralSuffix(diffTotalHours)} ago";
             }
 
             if (diff.TotalMinutes > 1)
             {
-                return (int)diff.TotalMinutes + " minutes ago";
+                var diffTotalMinutes = (int)diff.TotalMinutes;
+                return $"{diffTotalMinutes} minute{PluralSuffix(diffTotalMinutes)} ago";
             }
 
             if (diff.TotalSeconds < 0)
@@ -109,7 +115,8 @@ namespace Milou.Deployer.Web.Core.Time
                 return Constants.NotAvailable;
             }
 
-            return (int)diff.TotalSeconds + " seconds ago";
+            var diffTotalSeconds = (int)diff.TotalSeconds;
+            return $"{diffTotalSeconds} second{PluralSuffix(diffTotalSeconds)} ago";
         }
     }
 }
