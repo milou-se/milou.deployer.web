fd0f48ea98df01139ef3886be241d23423bc09ba
Niklas Lundberg <info@nlundberg.se>
2019-05-20 21:52:06 +0200

    Added ananymous attribute to

--- src/Milou.Deployer.Web.IisHost/Areas/Startup/StartupController.cs
+++ src/Milou.Deployer.Web.IisHost/Areas/Startup/StartupController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Milou.Deployer.Web.Core.Startup;
 
 namespace Milou.Deployer.Web.IisHost.Areas.Startup
 {
+    [AllowAnonymous]
     [Area(StartupConstants.AreaName)]
     public class StartupController : Controller
     {
