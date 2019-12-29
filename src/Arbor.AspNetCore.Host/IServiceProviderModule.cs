228186b4a677e5a7d228512097bc2c159addeaec
Niklas Lundberg <info@nlundberg.se>
2019-12-29 21:30:11 +0100

    Extracted more code.

--- src/Arbor.AspNetCore.Host/IServiceProviderModule.cs
+++
@@ -1,9 +0,0 @@
-﻿using Arbor.AspNetCore.Host.Hosting;
-
-namespace Arbor.AspNetCore.Host
-{
-    public interface IServiceProviderModule
-    {
-        void Register(ServiceProviderHolder serviceProviderHolder);
-    }
-}
