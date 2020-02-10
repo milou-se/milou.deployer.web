97a43d9c9ec3c0a9af5d45255eb0fe30c9d15dac
Niklas Lundberg <niklas.lundberg@milou.se>
2020-02-10 19:40:09 +0100

    Fixed test.

--- src/Milou.Deployer.Web.Tests.Unit/WhenSerializingADeploymentTarget.cs
+++ src/Milou.Deployer.Web.Tests.Unit/WhenSerializingADeploymentTarget.cs
@@ -1,4 +1,5 @@
-﻿using Milou.Deployer.Web.Core.Deployment;
+﻿using System;
+using Milou.Deployer.Web.Core.Deployment;
 using Milou.Deployer.Web.Core.Json;
 using Newtonsoft.Json;
 using Xunit;
@@ -32,8 +33,37 @@ namespace Milou.Deployer.Web.Tests.Unit
 
             Assert.NotNull(deserialized);
 
-            Assert.Null(deserialized.NuGet.NuGetPackageSource);
-            Assert.Null(deserialized.NuGet.NuGetConfigFile);
+            Assert.Null(deserialized.NuGet);
+            Assert.Null(deserialized.NuGet?.NuGetPackageSource);
+            Assert.Null(deserialized.NuGet?.NuGetConfigFile);
+        }
+
+        [Fact]
+        public void ItShouldBeDeserializableWithNuget()
+        {
+            var target = new DeploymentTarget("myid", "myName", "tool",
+                nuget: new TargetNuGetSettings
+                {
+                    NuGetConfigFile = "123",
+                    NuGetPackageSource = "source-abc",
+                    PackageListTimeout = TimeSpan.FromMinutes(1)
+                });
+
+            var json = JsonConvert.SerializeObject(target,
+                Formatting.Indented,
+                new JsonSerializerSettings().UseCustomConverters());
+
+            _testOutputHelper.WriteLine(json);
+
+            var deserialized = JsonConvert.DeserializeObject<DeploymentTarget>(json);
+
+            _testOutputHelper.WriteLine(deserialized.ToString());
+
+            Assert.NotNull(deserialized);
+
+            Assert.NotNull(deserialized.NuGet);
+            Assert.NotNull(deserialized.NuGet?.NuGetPackageSource);
+            Assert.NotNull(deserialized.NuGet?.NuGetConfigFile);
         }
     }
 }
